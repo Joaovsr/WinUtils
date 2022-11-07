db0b1113f36e00c1ce0640e33d4fbdf619284198
João Vinicius Rodrigues <48166817+Joaovsr@users.noreply.github.com>
2022-11-07 15:10:37 -0300

    up

--- create-restore-point.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "Creating Restore Point incase something bad happens"
-Enable-ComputerRestore -Drive "C:\"
-Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
--- privacy-and-performance.ps1
+++ privacy-and-performance.ps1
@@ -1,19 +1,15 @@
+Write-Host "Creating Restore Point incase something bad happens"
+Enable-ComputerRestore -Drive "C:\"
+Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
+
 Write-Host "Running O&O Shutup with Recommended Settings"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/ChrisTitusTech/win10script/master/ooshutup10.cfg" -Destination ooshutup10.cfg
 Start-BitsTransfer -Source "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -Destination OOSU10.exe
 ./OOSU10.exe ooshutup10.cfg /quiet
 
-#Need to be Fixed
-#Write-Host "Disabling GameDVR..."
-#Set-ItemProperty -Path "HKLM:\System\GameConfigStore" -Name "GameDVR_DXGIHonorFSEWindowsCompatible" -Type Hex -Value 00000000
-#Set-ItemProperty -Path "HKLM:\System\GameConfigStore" -Name "GameDVR_HonorUserFSEBehaviorMode" -Type Hex -Value 00000000
-#Set-ItemProperty -Path "HKLM:\System\GameConfigStore" -Name "GameDVR_EFSEFeatureFlags" -Type Hex -Value 00000000
-#Set-ItemProperty -Path "HKLM:\System\GameConfigStore" -Name "GameDVR_Enabled" -Type DWord -Value 00000000
-
 Write-Host "Showing known file extensions..."
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Type DWord -Value 0
-
 
 Write-Host "Set Services to Manual ..."
 
@@ -110,3 +106,102 @@ foreach ($service in $services) {
 	# Write-Host "Setting $service StartupType to Manual"
 	Get-Service -Name $service -ErrorAction SilentlyContinue | Set-Service -StartupType Manual
 }
+
+$Bloatware = @(
+		#Unnecessary Windows 10 AppX Apps
+		"Microsoft.3DBuilder"
+		"Microsoft.Microsoft3DViewer"
+		"Microsoft.AppConnector"
+		"Microsoft.BingFinance"
+		"Microsoft.BingNews"
+		"Microsoft.BingSports"
+		"Microsoft.BingTranslator"
+		"Microsoft.BingWeather"
+		"Microsoft.BingFoodAndDrink"
+		"Microsoft.BingHealthAndFitness"
+		"Microsoft.BingTravel"
+		"Microsoft.MinecraftUWP"
+		"Microsoft.GamingServices"
+		# "Microsoft.WindowsReadingList"
+		"Microsoft.GetHelp"
+		"Microsoft.Getstarted"
+		"Microsoft.Messaging"
+		"Microsoft.Microsoft3DViewer"
+		"Microsoft.MicrosoftSolitaireCollection"
+		"Microsoft.NetworkSpeedTest"
+		"Microsoft.News"
+		"Microsoft.Office.Lens"
+		"Microsoft.Office.Sway"
+		"Microsoft.Office.OneNote"
+		"Microsoft.OneConnect"
+		"Microsoft.People"
+		"Microsoft.Print3D"
+		"Microsoft.SkypeApp"
+		"Microsoft.Wallet"
+		"Microsoft.Whiteboard"
+		"Microsoft.WindowsAlarms"
+		"microsoft.windowscommunicationsapps"
+		"Microsoft.WindowsFeedbackHub"
+		"Microsoft.WindowsMaps"
+		"Microsoft.WindowsPhone"
+		"Microsoft.WindowsSoundRecorder"
+		"Microsoft.XboxApp"
+		"Microsoft.ConnectivityStore"
+		"Microsoft.CommsPhone"
+		"Microsoft.ScreenSketch"
+		"Microsoft.Xbox.TCUI"
+		"Microsoft.XboxGameOverlay"
+		"Microsoft.XboxGameCallableUI"
+		"Microsoft.XboxSpeechToTextOverlay"
+		"Microsoft.MixedReality.Portal"
+		"Microsoft.XboxIdentityProvider"
+		"Microsoft.ZuneMusic"
+		"Microsoft.ZuneVideo"
+		#"Microsoft.YourPhone"
+		"Microsoft.Getstarted"
+		"Microsoft.MicrosoftOfficeHub"
+		
+		#Sponsored Windows 10 AppX Apps
+		#Add sponsored/featured apps to remove in the "*AppName*" format
+		"*EclipseManager*"
+		"*ActiproSoftwareLLC*"
+		"*AdobeSystemsIncorporated.AdobePhotoshopExpress*"
+		"*Duolingo-LearnLanguagesforFree*"
+		"*PandoraMediaInc*"
+		"*CandyCrush*"
+		"*BubbleWitch3Saga*"
+		"*Wunderlist*"
+		"*Flipboard*"
+		"*Twitter*"
+		"*Facebook*"
+		"*Royal Revolt*"
+		"*Sway*"
+		"*Speed Test*"
+		"*Dolby*"
+		"*Viber*"
+		"*ACGMediaPlayer*"
+		"*Netflix*"
+		"*OneCalendar*"
+		"*LinkedInforWindows*"
+		"*HiddenCityMysteryofShadows*"
+		"*Hulu*"
+		"*HiddenCity*"
+		"*AdobePhotoshopExpress*"
+		"*HotspotShieldFreeVPN*"
+		
+		#Optional: Typically not removed but you can if you need to
+		"*Microsoft.Advertising.Xaml*"
+		#"*Microsoft.MSPaint*"
+		#"*Microsoft.MicrosoftStickyNotes*"
+		#"*Microsoft.Windows.Photos*"
+		#"*Microsoft.WindowsCalculator*"
+		#"*Microsoft.WindowsStore*"
+)
+
+Write-Host "Removing Bloatware"
+foreach ($Bloat in $Bloatware) {
+	Get-AppxPackage -Name $Bloat| Remove-AppxPackage
+		Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $Bloat | Remove-AppxProvisionedPackage -Online
+		Write-Host "Trying to remove $Bloat."
+}
+Write-Host "Finished Removing Bloatware Apps"
